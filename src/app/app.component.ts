45cf1c06e6aa526eeaa833d25a5bb41f6ad7d147
Travis Mix <travismix1980@gmail.com>
2021-02-20 15:49:57 -0800

    Pagination complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,36 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'At the Beach',
+      url:
+        'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+    {
+      title: 'Space',
+      url:
+        'https://images.unsplash.com/photo-1451187580459-43490279c0fa?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80',
+    },
+    {
+      title: 'Having Some Fun',
+      url:
+        'https://images.unsplash.com/photo-1511882150382-421056c89033?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1351&q=80',
+    },
+    {
+      title: 'My Favorite Car',
+      url:
+        'https://th.bing.com/th/id/OIP.8klHhkQeaVYm0TbGSwxdiQHaFk?pid=ImgDet&rs=1',
+    },
+    {
+      title: 'Gaming Setup',
+      url:
+        'https://images.unsplash.com/photo-1587202372616-b43abea06c2a?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1350&q=80',
+    },
+  ];
+
 }
